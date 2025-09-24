0ccbde2861a9a166938f86183eca7bef8c64b6eb
Oleg <al.neskor@gmail.com>
2025-09-24 17:07:22 +0300

    помилки з кометаря ментора виправив

--- src/filetojson/FileTxtToJson.java
+++ src/filetojson/FileTxtToJson.java
@@ -16,20 +16,19 @@ public class FileTxtToJson {
     public static void main(String[] args) throws IOException {
         ArrayList<User> users = new ArrayList<>();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        FileWriter fw = new FileWriter("user.json");
-        BufferedReader br = new BufferedReader(new FileReader("file_user.txt"));
-        br.readLine();
-        String line =br.readLine();
-        while (line!=null){
-            String[] spl = line.split("\\s+");
-            //System.out.println(Arrays.toString(spl)+"////");
-            User user = new User(spl[0], Integer.parseInt(spl[1]));
-            users.add(user);
-           line = br.readLine();
+        try(FileWriter fw = new FileWriter("user.json");
+        BufferedReader br = new BufferedReader(new FileReader("file_user.txt"))) {
+            br.readLine();
+            String line = br.readLine();
+            while (line != null) {
+                String[] spl = line.split("\\s+");
+                User user = new User(spl[0], Integer.parseInt(spl[1]));
+                users.add(user);
+                line = br.readLine();
+            }
+            gson.toJson(users, fw);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        br.close();
-        gson.toJson(users, fw);
-        fw.close();
     }
-    
 }
--- src/filetojson/User.java
+++ src/filetojson/User.java
@@ -3,7 +3,6 @@ package filetojson;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
     private String name;
     private int age;
     private String nameColum;
